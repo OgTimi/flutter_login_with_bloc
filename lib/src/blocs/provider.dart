8013cfea0c6a9e8b306bed413005c883f7a2eeca
Software Engineering <saeddev@bitbucket.org>
2020-05-25 16:46:27 +0100

    using bloc on a global scale

---
+++ lib/src/blocs/provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'bloc.dart';
+
+class Provider extends InheritedWidget{
+  bool updateShouldNotify(_) => true;
+
+  final bloc = Bloc();
+
+  Provider(Key key, Widget child) : super(key: key, child: child);
+
+  static Bloc of(BuildContext context){
+    // return (context.dependOnInheritedWidgetOfExactType() as Provider).bloc;
+    return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
+  }
+}
